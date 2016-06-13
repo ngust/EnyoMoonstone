dfd8b39997360286056de3059b624fae3c5f89de
Nicholas Gust <nicholasgust@gmail.com>
2016-06-13 12:55:38 -0700

    added search function and img url generator

--- source/data/data.js
+++ source/data/data.js
@@ -10,10 +10,56 @@ enyo.kind({
         urlRoot: "https://api.flickr.com/services/rest/",
         fetch: function(rec, opts) {
             opts.callbackName = "jsoncallback";
-            opts.params = {};
+            opts.params = enyo.clone(rec.params);
             opts.params.api_key = "2a21b46e58d207e4888e1ece0cb149a5";
             opts.params.format = "json";
             this.inherited(arguments);
+        }
+    });
+
+enyo.kind({
+    name: "flickr.SearchCollection",
+    kind: "enyo.Collection",
+    model: "flickr.ImageModel",
+    source: "flickr",
+    options: { parse: true },
+    published: {
+        searchText: null,
+    },
+    searchTextChanged: function() {
+        this.empty({destroy: true});
+        this.fetch();
+    },
+    fetch: function(opts) {
+        this.params = {
+            method: "flickr.photos.search",
+            sort: "interestingness-desc",
+            per_page: 50,
+            text: this.searchText
+        };
+        return this.inherited(arguments);
+    },
+    parse: function(data) {
+    return data && data.photos && data.photos.photo;
+  }
+});
+
+enyo.kind({
+        name: "flickr.ImageModel",
+        kind: "enyo.Model",
+        computed: [
+            {method: "thumbnail", path: ["farm", "server", "id", "secret"]},
+            {method: "original", path: ["farm", "server", "id", "secret"]}
+        ],
+        thumbnail: function() {
+            return "https://farm" + this.get("farm") +
+                ".static.flickr.com/" + this.get("server") +
+                "/" + this.get("id") + "_" + this.get("secret") + "_m.jpg";
+        },
+        original: function() {
+            return "https://farm" + this.get("farm") +
+                ".static.flickr.com/" + this.get("server") +
+                "/" + this.get("id") + "_" + this.get("secret") + ".jpg";
         }
     });
 
